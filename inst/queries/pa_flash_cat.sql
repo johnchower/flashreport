9910c5c0231fcad4ebcb0575bf8940f0b9042a6d
johnchower <johnchower@gmail.com>
2017-03-17 09:09:38 -0600

    Misc. cleanup

---
+++ inst/queries/pa_flash_cat.sql
@@ -0,0 +1,4 @@
+CREATE TEMPORARY TABLE pa_flash_cat
+  (platform_action VARCHAR
+    , flash_report_category VARCHAR)
+;
--- scratch_work/create_pa_category_string.r
+++ scratch_work/create_pa_category_string.r
@@ -1,8 +1,7 @@
-# platform_action_categories <- 
-pa_classes <- 
+pa_classes <-
   paste0(
     readLines('~/Projects/flashreport/scratch_work/pa_classes')
     , collapse = ""
-  ) 
+  )
 
 devtools::use_data(pa_classes, overwrite = T)
